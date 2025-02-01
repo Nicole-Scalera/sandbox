c74eb153a9b8a32195adfda7c4e3eb78dbf2993c
Nicole Scalera <nikkiraescalera@gmail.com>
2025-02-01 18:58:42 -0500

    Created C# OOP Text Adventure Project
    Overview
    - Created a new C# Console App Project in Visual Studio 2022.
    - Runs on .NET 8.0 (Long Term Support).
    
    In-depth Details
    - I will break down the details of this commit below.
    
    A. Purposes of This Project
    - I created this project in an attempt to teach myself more about object-oriented programming in C#.
    - I learned and practiced OOP concepts through several Java classes.
    - I believe this helped me teach C# to myself.
    - However, as much as I love documentation, I really like to go follow a lesson plan through textbooks, videos, and any other learning materials I can get my hands on.
    - Likewise, I am going through a lesson on OOP in C# now to brush up on my skills.
    - Hopefully this will allow me to optimize and improve my other projects.
    
    B. Commit Contents
    - In this commit, I created a new solution and project named oop-text-adventure.
    - Set up the Program.cs file.
         - This file prompts the user for their name.
         - It grabs the contents of their response and sets it to a variable called name.
         - If their response is empty, it will just set the name to "No Name"
              - It uses {0} as a placeholder to grab the first variable created.
         - The system then welcomes them to the program.

---
+++ C#/OOP/oop-text-adventure/Program.cs
@@ -0,0 +1,15 @@
+﻿// Prompt user for name
+Console.WriteLine("Hello, what's your name?");
+
+// Get user input
+var name = Console.ReadLine();
+
+// If the user dosen't return anything,
+if (name == String.Empty)
+{
+    // set the name to "No Name"
+    name = "No Name";
+}
+
+// Respond to the user
+Console.WriteLine("Welcome {0} to your OOP adventure!", name);
