40856ccbbdbf052cba440a0b8739dba7ded8f6a2
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-03-13 10:34:40 -0400

    Created dotNETConfigCat2
    Overview
    - Uploading a series of edits I made to the dotNETConfigCat1 project (9e3d944).
    
    In-depth Details
    - This commit regards a series of miscellaneous edits made in an attempt to address #10.
    - I will break down the edits below.
    
    A. Changes Made
    - Most of the lines added in this push are copied from the official ConfigCat .NET tutorial.
    - For example, the "var client" on line utilizes the SDK to connect to my dashboard where I control my feature flags.
    - However, I kept getting the following error code:
         - CS8370: Feature 'top-level statements' is not available in C# 7.3. Please use language version 9.0 or greater.
    
    B. Fixing the Issue
    - I managed to fix this by changing my project type in Visual Studio Community 2022:
         - Previous: Console App (.NET Framework)
         - New: Console App
    - As it turns out, the standard Console App still runs on .NET, despite it not being in the title.
    	- Created: dotNETConfigCat2
    - Likewise, I'm committing both the new project, as well as the old one, just to show the difference.
         - Also added the ConfigCat.Client using the Nuget Package Manager.

--- Feature Flags/.NETConfigCat1/dotNETConfigCat1/dotNETConfigCat1/Program.cs
+++ Feature Flags/.NETConfigCat1/dotNETConfigCat1/dotNETConfigCat1/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConfigCat.Client;
 
-namespace dotNETConfigCat1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+// SDK to connect to ConfigCat dashboard
+var client = ConfigCatClient.Get("configcat-sdk-1/D0vdCEFN30uPhcE4NpqzEQ/WUffFKazcEOC1-ZXIHmSfg");
+
+// Sample code from dashboard
+var isMyFirstFeatureEnabled = await client.GetValueAsync("isMyFirstFeatureEnabled", false);
+var dotnetconfigcat1feat = await client.GetValueAsync("dotnetconfigcat1feat", false);
+
+// Write to console if flag is on/off (bool)
+Console.WriteLine("isMyFirstFeatureEnabled's value from ConfigCat: " + isMyFirstFeatureEnabled);
+Console.WriteLine("dotnetconfigcat1feat's value from ConfigCat: " + dotnetconfigcat1feat);
