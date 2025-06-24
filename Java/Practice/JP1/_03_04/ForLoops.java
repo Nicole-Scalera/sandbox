89241ee36f128c2b747d663eaac93ad2f47be411
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-06-24 14:30:28 -0400

    Updated ForLoops.java
    Overview
    - Updated ForLoops.java in the _03_04 folder.
    
    In-depth Details
    - This follows the control flow methods introduced in f60c5e7.
    - In this commit, I completed the standard for-loops exercise.
    - Note: I didn't create a separate branch for these last few commits.
         - I may need to add a README.md at the directory of each course I complete to avoid confusion.

--- Java/Practice/JP1/_03_04/ForLoops.java
+++ Java/Practice/JP1/_03_04/ForLoops.java
@@ -4,11 +4,20 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
+    for (int i = 1; i <= 5; i++) {
+      System.out.println("I love you! | Iteration #" + i);
+      // TIP: You can also do (int i = 0; i < 5; i++) to get the same result.
+    }
 
     // Write a for loop that prints out the numbers 1 to 10
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for (int i = 10; i >= 1; i--) {
+      System.out.println(i);
+    }
 
   }
 
