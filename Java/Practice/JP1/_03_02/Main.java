383533c7a79e7dce5b57e133b2110a91067b699b
Nicole Scalera <85967480+Nicole-Scalera@users.noreply.github.com>
2025-06-24 14:15:54 -0400

    Updated GradingSystem.java and Main.java
    Overview
    - Updated GradingSystem.java and Main.java in the _03_03 folder.
    
    In-depth Details
    - In this commit, I completed the Java course's If/else statement practice.
         - 1/3 of the control flows mentioned in f60c5e7.
    - The two files are connected in that Main.java actually runs the program.
    - Main.java creates an instance of GradingSystem.java at the beginning to use.
         - The logic is then passed through the contents of GradingSystem.java.

--- Java/Practice/JP1/_03_02/Main.java
+++ Java/Practice/JP1/_03_02/Main.java
@@ -3,11 +3,19 @@ package _03_02;
 public class Main {
 
   public static void main(String[] args) {
+    // Create a new instance of the GradingSystem
     GradingSystem gradingSystem = new GradingSystem();
-    int percentage = 85;
+    int percentage = 85; // Set the percentage grade here
+
+    // These three lines simply print out information about the student's
+    // grade (percentage, whether or not they passed, and the letter grade).
     System.out.println("Percentage: " + percentage);
     System.out.println("Pass: " + gradingSystem.isAPass(percentage));
     System.out.println("Grade: " + gradingSystem.getGrade(percentage));
+
+    // The below line passes two parameters into the retakeMessage method in
+    // GradingSystem.java, which influences whether or not they can retake the
+    // exam, and will alter the output accordingly.
     System.out.println(gradingSystem.retakeMessage(percentage, true));
   }
 
